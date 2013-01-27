3660cdbdbbeec3a2910a1d1a9d2bae0cafd91108
Hendrik Speidel <speidel@schnapptack.de>
2013-01-27 12:30:27 +0100

    updated author email

--- featuremonkey/__init__.py
+++ featuremonkey/__init__.py
@@ -3,4 +3,4 @@ from featuremonkey.composer import compose, compose_later,\
      select, select_equation
 
 __version__ = '0.1'
-__author__ = 'Hendrik Speidel <speidel@schnapptack.de>'
+__author__ = 'Hendrik Speidel <hendrik@schnapptack.de>'
