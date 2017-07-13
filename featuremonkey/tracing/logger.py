f6230ed078dd6d4e2a7335b84b6dc642e9e7656f
Dominik Halle <dominik.halle@schnapptack.de>
2017-07-13 13:26:15 +0200

    fixed error in logger when new_value/old_value was a lazy translation

--- featuremonkey/tracing/logger.py
+++ featuremonkey/tracing/logger.py
@@ -1,4 +1,5 @@
-from __future__ import print_function
+# coding: utf-8
+from __future__ import print_function, unicode_literals
 import copy
 
 
@@ -27,13 +28,34 @@ class NullOperationLogger(object):
 class OperationLogger(NullOperationLogger):
     operation_log = OPERATION_LOG
 
+    @staticmethod
+    def _get_lazy_translation_value(value):
+        """
+        Special handling for top level (not nested in other data structures) lazy translation objects.
+        This should prevent the translation process and so there is no import guard error.
+        It should cover most cases of translation usage.
+        :param value:
+        :return:
+        """
+        if hasattr(value, '_proxy____args'):
+            translation_args = getattr(value, '_proxy____args')
+            return "".join(translation_args)
+
     def log(self, operation=dict(), new_value="", old_value=""):
         operation['new_value'] = copy.deepcopy(new_value)
         operation['old_value'] = copy.deepcopy(old_value)
         self.operation_log.append(operation)
 
     def log_new_value(self, operation=dict(), new_value=""):
-        self.operation_log[self.operation_log.index(operation)]['new_value'] = copy.deepcopy(new_value)
+        translation_value = self._get_lazy_translation_value(new_value)
+        if translation_value:
+            self.operation_log[self.operation_log.index(operation)]['new_value'] = translation_value
+        else:
+            self.operation_log[self.operation_log.index(operation)]['new_value'] = copy.deepcopy(new_value)
 
     def log_old_value(self, operation=dict(), old_value=""):
-        self.operation_log[self.operation_log.index(operation)]['old_value'] = copy.deepcopy(old_value)
+        translation_value = self._get_lazy_translation_value(old_value)
+        if translation_value:
+            self.operation_log[self.operation_log.index(operation)]['old_value'] = translation_value
+        else:
+            self.operation_log[self.operation_log.index(operation)]['old_value'] = copy.deepcopy(old_value)
